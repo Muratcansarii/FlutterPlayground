0fba29af946e3a98ceec1cfb2ef91c5a14979672
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2024-03-11 01:03:41 +0300

    changes

---
+++ lib/theme_stuff8/colours.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Colours {
+  static const swatch = {
+    50: Color.fromRGBO(136, 14, 79, .1),
+    100: Color.fromRGBO(136, 14, 79, .2),
+    200: Color.fromRGBO(136, 14, 79, .3),
+    300: Color.fromRGBO(136, 14, 79, .4),
+    400: Color.fromRGBO(136, 14, 79, .5),
+    500: Color.fromRGBO(136, 14, 79, .6), // The primary color
+    600: Color.fromRGBO(136, 14, 79, .7),
+    700: Color.fromRGBO(136, 14, 79, .8),
+    800: Color.fromRGBO(136, 14, 79, .9),
+    900: Color.fromRGBO(136, 14, 79, 1),
+  };
+
+  static const primarySwatch = {
+    50: Color(0xFF7E57C2),
+    100: Color(0xFF7E57C2),
+    200: Color(0xFF7E57C2),
+    300: Color(0xFF7E57C2),
+    400: Color(0xFF7E57C2),
+    500: Color(0xFF7E57C2),
+    600: Color(0xFF7E57C2),
+    700: Color(0xFF7E57C2),
+    800: Color(0xFF7E57C2),
+    900: Color(0xFF7E57C2),
+
+  };
+
+  static const PRIMARY_COLOR = Color(0xFF7E57C2);
+  static const PRIMARY_DARK_COLOR = Color(0xFF4527A0);
+  static const PRIMARY_SWATCH = MaterialColor(0xFF7E57C2, swatch);
+  static const PRIMARY_DARK_SWATCH = MaterialColor(0xFF4527A0, swatch);
+
+
+
+}
