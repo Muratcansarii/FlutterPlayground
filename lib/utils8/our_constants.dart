4a0dea329907f7c30e9a57e95387dcc8d1893973
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2024-03-11 01:03:28 +0300

    changes

---
+++ lib/utils8/our_constants.dart
@@ -0,0 +1,14 @@
+class SettingsConstants {
+  static const isDarkKey = "com.abing.isDark";
+  static const language = "com.abing.language";
+  static const themeColor = "com.abing.themeColor";
+}
+
+class PomodoroModelConstants {
+  static const modelKey = "com.abing.modelKey";
+}
+
+class NotificationConstants {
+  static const channelId = "0";
+  static const channelName = "com.abing.localNotification";
+}
