855e6ef136131995d830c11f331dbeb40aa4e968
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2024-04-06 18:47:14 +0300

    changes

--- android/app/src/main/kotlin/com/example/flutter_playground/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_playground/MainActivity.kt
@@ -3,3 +3,7 @@ package com.example.flutter_playground
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
