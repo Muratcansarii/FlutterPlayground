182f014ed406c9ebe32139d32520a60c8bc2779e
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2024-04-06 18:50:14 +0300

    changes

--- android/app/src/main/kotlin/com/example/flutter_playground/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_playground/MainActivity.kt
@@ -31,4 +31,27 @@ class MainActivity: FlutterActivity()
     // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
+
+
+    // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+    // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  
+    // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+    // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  
+    // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+    // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  
   
