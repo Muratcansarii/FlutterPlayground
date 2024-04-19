eb3355837b55cbea243c605e0292a820577240c9
Muratcan Sarı <64330835+Muratcansarii@users.noreply.github.com>
2024-04-20 00:34:10 +0300

    changes

--- lib/models/tasks.dart
+++
@@ -1,9 +0,0 @@
-class Task{
-  final String taskName;
-  bool isDone;
-  String estPomo;
-  Task({required this.taskName,required this.estPomo,this.isDone=false});
-
-  void toggleDone()=>isDone=!isDone;
-
-}
